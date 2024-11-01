a4dd674ce034928c89359841716ce983944bc76d
Breno-Saraiva <breno.saraiva@icc.org.br>
2024-11-01 16:14:45 -0300

    feat: create page questionnaire

--- src/modules/questionnaire/page/index.tsx
+++ src/modules/questionnaire/page/index.tsx
@@ -1,5 +1,23 @@
+import logo from "@/shared/assets/logo.png";
+import { IoIosLogOut } from "react-icons/io";
+import { Link } from "react-router-dom";
+
 const Questionnaire = () => {
-  return <div>hello world</div>;
+  return (
+    <>
+      <header className="py-4 px-8 bg-[#12642A] flex items-center justify-between">
+        <img className="w-24" src={logo} alt="logo" />
+        <div className="flex gap-8 items-center">
+          <h1 className="text-white text-lg font-bold w">
+            Olá, <span>Carlos</span>
+          </h1>
+          <Link to={"/"}>
+            <IoIosLogOut color="white" size={30} />
+          </Link>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export { Questionnaire };
