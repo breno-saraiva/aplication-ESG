e344c4b8278c2016824994aeb710143b5efc2922
Breno-Saraiva <breno.saraiva@icc.org.br>
2024-11-01 14:41:11 -0300

    feat: create login page

---
+++ src/modules/login/components/register/index.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/shared/components/ui/button";
+import { Input } from "@/shared/components/ui/input";
+import { Label } from "@/shared/components/ui/label";
+
+const Register = () => {
+  return (
+    <div className="p-4">
+      <div className="my-4 flex items-center justify-center">
+        <h1 className="text-white text-lg font-bold">Crie Sua conta</h1>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div>
+          <Label className="text-white">Nome do Usuário:</Label>
+          <Input className="bg-white" placeholder="Digite seu usuário..." />
+        </div>
+        <div>
+          <Label className="text-white">Senha:</Label>
+          <Input className="bg-white" placeholder="Digite sua senha..." />
+        </div>
+        <div>
+          <Label className="text-white">Confirme a Senha:</Label>
+          <Input
+            className="bg-white"
+            placeholder="Digite a senha novamente..."
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <Button className="text-lg bg-[#FF6F20]">Cadastrar</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Register };
--- src/modules/login/page/index.tsx
+++ src/modules/login/page/index.tsx
@@ -1,5 +1,50 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/shared/components/ui/tabs";
+import logo from "@/shared/assets/logo.png";
+import { Register } from "../components/register";
+import { SignIn } from "../components/signIn";
+
 const Login = () => {
-  return <div>hello world</div>;
+  return (
+    <div className="w-full h-screen bg-[#12642A] flex ">
+      <div className="m-auto  flex flex-col">
+        <div className="flex flex-col gap-4">
+          <img className="text-2xl" src={logo} alt="Logo" />
+          <p className="text-white text-lg font-medium flex flex-col justify-center items-center">
+            Seu Guia de uma vida <br />{" "}
+            <span className="text-xl">Sustentável</span>
+          </p>
+        </div>
+        <div>
+          <p></p>
+        </div>
+      </div>
+      <div className="m-auto">
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList className="bg-green-600/70 text-white">
+            <TabsTrigger value="signIn">Login</TabsTrigger>
+            <TabsTrigger value="register">Cadastre-se</TabsTrigger>
+          </TabsList>
+          <TabsContent
+            className="bg-green-600/70 rounded-lg p-4"
+            value="signIn"
+          >
+            <SignIn />
+          </TabsContent>
+          <TabsContent
+            className="bg-green-600/70 rounded-lg p-4"
+            value="register"
+          >
+            <Register />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export { Login };
