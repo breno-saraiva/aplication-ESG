e40899378db9ae40b1ef6e128cf8ab9b9eb8d00c
Breno-Saraiva <breno.saraiva@icc.org.br>
2024-11-04 18:00:51 -0300

    feat: create step progress page

--- src/modules/questionnaire/page/index.tsx
+++ src/modules/questionnaire/page/index.tsx
@@ -1,4 +1,5 @@
 import logo from "@/shared/assets/logo.png";
+import { Checkbox } from "@/shared/components/ui/checkbox";
 import { IoIosLogOut } from "react-icons/io";
 import { Link } from "react-router-dom";
 
@@ -16,8 +17,153 @@ const Questionnaire = () => {
           </Link>
         </div>
       </header>
-      <main>
-        <h1>Hello world</h1>
+      <main className="w-full px-24 mt-8 flex flex-col gap-8">
+        <div className="flex justify-center items-center">
+          <h1 className="font-semibold text-4xl text-green-800">
+            Sustentabilidade
+          </h1>
+        </div>
+        <div></div>
+        <div className="flex flex-col gap-4">
+          <div className="w-full h-2 rounded-full bg-green-500 flex">
+            {/* 0% */}
+            <div className="flex items-center">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 10% */}
+            <div className="flex items-center justify-end bg-[ProgresProps[state]] w-full h-2 rounded-r-full">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 20% */}
+            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 30% */}
+            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 40% */}
+            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 50% */}
+            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 60% */}
+            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 70% */}
+            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 80% */}
+            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
+              <div className="border-4 border-green-500 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 90% */}
+            <div className="flex items-center justify-end bg-zinc-300 w-full h-2 rounded-r-full">
+              <div className="border-4 border-zinc-300 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+            {/* 100% */}
+            <div className="flex items-center justify-end bg-zinc-300 w-full h-2 rounded-r-full">
+              <div className="border-4 border-zinc-300 rounded-full bg-white">
+                <div className="border-4 border-white rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex justify-between">
+            <h1>0%</h1>
+            <h1>10%</h1>
+            <h1>20%</h1>
+            <h1>30%</h1>
+            <h1>40%</h1>
+            <h1>50%</h1>
+            <h1>60%</h1>
+            <h1>70%</h1>
+            <h1>80%</h1>
+            <h1>90%</h1>
+            <h1>100%</h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <Checkbox id="1" />
+            <label htmlFor="1">Demorou menos 15 minutos no banho</label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="2" />
+            <label htmlFor="2">
+              Desligou a torneira da pia enquanto escovava os dentes
+            </label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="3" />
+            <label htmlFor="3">
+              Fechou bem o registro, para evitar vazamentos
+            </label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="4" />
+            <label htmlFor="4">
+              Fechou a torneira enquanto higienizava a louça
+            </label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="5" />
+            <label htmlFor="5">
+              Juntou roupa o suficiente para não ter que fazer múltiplas
+              lavagens
+            </label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="6" />
+            <label htmlFor="6">
+              Utilizou lâmpadas de LED para economizar energia
+            </label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="7" />
+            <label htmlFor="7">
+              Reutilizou sacolas plásticas ou usou sacolas reutilizáveis
+            </label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="8" />
+            <label htmlFor="8">Separou o lixo para reciclagem</label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="9" />
+            <label htmlFor="9">Optou por produtos com menos embalagens</label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="10" />
+            <label htmlFor="10">
+              Evitou o uso de descartáveis, preferindo itens reutilizáveis
+            </label>
+          </div>
+        </div>
       </main>
     </>
   );
