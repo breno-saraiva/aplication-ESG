ef29a9416a528136cd3bf7cc693823e7fc47620d
Breno-Saraiva <breno.saraiva@icc.org.br>
2024-11-06 18:13:20 -0300

    feat: create checkbox logic

---
+++ src/modules/questionnaire/mock/questionare.ts
@@ -0,0 +1,44 @@
+const questionare = [
+  {
+    item: 1,
+    descricao: "Demorou menos 15 minutos no banho",
+  },
+  {
+    item: 2,
+    descricao: " Desligou a torneira da pia enquanto escovava os dentes",
+  },
+  {
+    item: 3,
+    descricao: "Fechou bem o registro, para evitar vazamentos",
+  },
+  {
+    item: 4,
+    descricao: "Fechou a torneira enquanto higienizava a louça",
+  },
+  {
+    item: 5,
+    descricao:
+      "Juntou roupa o suficiente para não ter que fazer múltiplas lavagens",
+  },
+  {
+    item: 6,
+    descricao: "Utilizou lâmpadas de LED para economizar energia",
+  },
+  {
+    item: 7,
+    descricao: "Reutilizou sacolas plásticas ou usou sacolas reutilizáveis",
+  },
+  {
+    item: 8,
+    descricao: "Separou o lixo para reciclagem",
+  },
+  {
+    item: 9,
+    descricao: "Optou por produtos com menos embalagens",
+  },
+  {
+    item: 10,
+    descricao: "Evitou o uso de descartáveis, preferindo itens reutilizáveis",
+  },
+];
+export { questionare };
--- src/modules/questionnaire/page/index.tsx
+++ src/modules/questionnaire/page/index.tsx
@@ -2,11 +2,72 @@ import logo from "@/shared/assets/logo.png";
 import { Checkbox } from "@/shared/components/ui/checkbox";
 import { IoIosLogOut } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { questionare } from "../mock/questionare";
+import { CheckedState } from "@radix-ui/react-checkbox";
+import { cn } from "@/shared/lib/utils";
 import { FaFaceSadTear } from "react-icons/fa6";
 import { FaFaceSmileBeam } from "react-icons/fa6";
 import { FaFaceGrinBeam } from "react-icons/fa6";
 
 const Questionnaire = () => {
+  const [feedBackFace, setFeedBackFace] = useState({ type: "sad", cont: 0 });
+
+  const feedBack: {
+    [key: string]: {
+      emotion: JSX.Element;
+      bg: string;
+      mensage: string;
+    };
+  } = {
+    sad: {
+      emotion: <FaFaceSadTear size={60} className="text-white" />,
+      bg: "bg-red-500",
+      mensage: "Você precisa cuidar do nosso planeta!",
+    },
+    fine: {
+      emotion: <FaFaceSmileBeam size={60} className="text-white" />,
+      bg: "bg-blue-700",
+      mensage: "Boa, você está tornando um mundo melhor",
+    },
+    happy: {
+      emotion: <FaFaceGrinBeam size={60} className="text-white" />,
+      bg: "bg-green-600",
+      mensage: "Uhull, você é um herói da sustentabilidade!",
+    },
+  };
+
+  function handleCheckbox(checked: CheckedState) {
+    setFeedBackFace((prev) => ({
+      ...prev,
+      cont: checked ? prev.cont + 1 : prev.cont - 1,
+      type:
+        prev.cont <= 4
+          ? "sad"
+          : prev.cont > 4 && prev.cont <= 7
+          ? "fine"
+          : "happy",
+    }));
+    console.log("========================", feedBackFace.cont);
+    console.log("========================", checked);
+  }
+
+  function changeFeedBack() {
+    if (feedBackFace.cont <= 4)
+      setFeedBackFace((prev) => ({
+        ...prev,
+        type:
+          prev.cont <= 4
+            ? "sad"
+            : prev.cont > 4 && prev.cont < 7
+            ? "fine"
+            : "happy",
+      }));
+  }
+
+  const checkboxes = Array.from({ length: 10 });
+  const divs = Array.from({ length: checkboxes.length });
+
   return (
     <>
       <header className="py-4 px-8 bg-[#12642A] flex items-center justify-between">
@@ -26,11 +87,17 @@ const Questionnaire = () => {
             Sustentabilidade
           </h1>
         </div>
-        <div className="mx-36 flex flex-col justify-center items-center gap-4 bg-red-500 px-12 py-2 rounded-3xl">
+        <div
+          className={cn(
+            `mx-36 flex flex-col justify-center items-center gap-4 ${
+              feedBack[feedBackFace.type].bg
+            } px-12 py-2 rounded-3xl`
+          )}
+        >
           <h1 className="text-white font-bold text-xl">
-            Você precisa cuidar do nosso planeta!
+            {feedBack[feedBackFace.type].mensage}
           </h1>
-          <FaFaceSadTear size={60} className="text-white" />
+          {feedBack[feedBackFace.type].emotion}
         </div>
         <div className="flex flex-col gap-4">
           <div className="w-full h-2 rounded-full bg-green-500 flex">
@@ -40,66 +107,27 @@ const Questionnaire = () => {
                 <div className="border-4 border-white rounded-full"></div>
               </div>
             </div>
-            {/* 10% */}
-            <div className="flex items-center justify-end bg-[ProgresProps[state]] w-full h-2 rounded-r-full">
-              <div className="border-4 border-green-500 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
+            {divs.map((_, index) => (
+              <div
+                className={cn(
+                  `flex items-center justify-end ${
+                    index < feedBackFace.cont ? "bg-green-500" : "bg-zinc-300"
+                  } w-full h-2 rounded-r-full`
+                )}
+              >
+                <div
+                  className={cn(
+                    `border-4 ${
+                      index < feedBackFace.cont
+                        ? "border-green-500"
+                        : "border-zinc-300"
+                    } rounded-full bg-white`
+                  )}
+                >
+                  <div className="border-4 border-white rounded-full"></div>
+                </div>
               </div>
-            </div>
-            {/* 20% */}
-            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
-              <div className="border-4 border-green-500 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
-            {/* 30% */}
-            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
-              <div className="border-4 border-green-500 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
-            {/* 40% */}
-            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
-              <div className="border-4 border-green-500 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
-            {/* 50% */}
-            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
-              <div className="border-4 border-green-500 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
-            {/* 60% */}
-            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
-              <div className="border-4 border-green-500 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
-            {/* 70% */}
-            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
-              <div className="border-4 border-green-500 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
-            {/* 80% */}
-            <div className="flex items-center justify-end bg-green-500 w-full h-2 rounded-r-full">
-              <div className="border-4 border-green-500 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
-            {/* 90% */}
-            <div className="flex items-center justify-end bg-zinc-300 w-full h-2 rounded-r-full">
-              <div className="border-4 border-zinc-300 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
-            {/* 100% */}
-            <div className="flex items-center justify-end bg-zinc-300 w-full h-2 rounded-r-full">
-              <div className="border-4 border-zinc-300 rounded-full bg-white">
-                <div className="border-4 border-white rounded-full"></div>
-              </div>
-            </div>
+            ))}
           </div>
           {/* contagem da porcentagem */}
           <div className="w-full flex justify-between">
@@ -116,66 +144,20 @@ const Questionnaire = () => {
             <h1>100%</h1>
           </div>
         </div>
-        <div className="flex">
-          <div className="w-full mt-12 flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-              <Checkbox id="1" />
-              <label htmlFor="1">Demorou menos 15 minutos no banho</label>
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="2" />
-              <label htmlFor="2">
-                Desligou a torneira da pia enquanto escovava os dentes
-              </label>
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="3" />
-              <label htmlFor="3">
-                Fechou bem o registro, para evitar vazamentos
-              </label>
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="4" />
-              <label htmlFor="4">
-                Fechou a torneira enquanto higienizava a louça
-              </label>
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="5" />
-              <label htmlFor="5">
-                Juntou roupa o suficiente para não ter que fazer múltiplas
-                lavagens
-              </label>
-            </div>
-          </div>
-          <div className="w-full m-12 flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-              <Checkbox id="6" />
-              <label htmlFor="6">
-                Utilizou lâmpadas de LED para economizar energia
-              </label>
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="7" />
-              <label htmlFor="7">
-                Reutilizou sacolas plásticas ou usou sacolas reutilizáveis
-              </label>
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="8" />
-              <label htmlFor="8">Separou o lixo para reciclagem</label>
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="9" />
-              <label htmlFor="9">Optou por produtos com menos embalagens</label>
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="10" />
-              <label htmlFor="10">
-                Evitou o uso de descartáveis, preferindo itens reutilizáveis
-              </label>
-            </div>
-          </div>
+        <div className="w-full mt-12 grid grid-cols-12 gap-2">
+          {questionare &&
+            questionare.map((item, index) => (
+              <div className="col-span-6 flex items-center gap-2">
+                <Checkbox
+                  id={`checkbox${index}`}
+                  onCheckedChange={(checked: CheckedState) => {
+                    handleCheckbox(checked);
+                    changeFeedBack();
+                  }}
+                />
+                <label htmlFor={`checkbox${index}`}>{item.descricao}</label>
+              </div>
+            ))}
         </div>
       </main>
     </>
